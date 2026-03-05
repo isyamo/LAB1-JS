33cb56efeb20453ab839a9f3464d3b95105390e9
MOUMAD Islam <islammoumad1718@gmail.com>
2026-03-05 20:06:11 +0000

    lab1JS

---
+++ lab-variables.js
@@ -0,0 +1,18 @@
+
+var petDog = 'Max';
+var petCat = 'ZOOER';
+
+console.log(petDog);
+console.log(petCat);
+
+console.log("Le nom de mon chien est : " + petDog);
+console.log("Le nom de mon chat est : " + petCat);
+
+var catSound = "roon";
+var dogSound = "ouh ";
+
+console.log(petDog, "dit", dogSound);
+console.log(petCat, "dit", catSound);
+
+catSound = "miaou";
+console.log(petCat, "dit maintenant", catSound);
